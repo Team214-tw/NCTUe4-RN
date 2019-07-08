cfedc0113eec9231fbcb79f80b144cf4271fb1fa
kaiiiz <at881005@gmail.com>
2019-07-08 20:00:18 +0800

    Add CourseAnnGeneralScreen

--- src/screens/course/Ann/AnnGeneral.tsx
+++ src/screens/course/Ann/AnnGeneral.tsx
@@ -1,34 +1,123 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import {
   View,
   Text,
-  Button,
-  StyleSheet
+  StyleSheet,
+  RefreshControl,
+  ListRenderItemInfo,
+  FlatList,
 } from 'react-native'
 import { NavigationScreenProp } from 'react-navigation';
+import NewE3ApiClient from '../../../client/NewE3ApiClient';
+import AsyncStorage from '@react-native-community/async-storage';
+import * as Progress from 'react-native-progress';
+import { ListItem } from 'react-native-elements';
 
 interface Props {
   navigation: NavigationScreenProp<States, Props>,
 }
 interface States {
-  
+  loading: boolean,
+  refreshing: boolean,
+  courseId: number,
+  AnnList: Array<{ key: string, data: ann_type, }>,
 }
 
-export default class CourseAnnGeneralScreen extends Component<Props> {
-
-  async componentDidMount() {
-    // console.log(this.props.navigation.dangerouslyGetParent().getParam('courseId'))
-  }
+export default class CourseAnnGeneralScreen extends Component<Props, States> {
 
   constructor(props: Props) {
     super(props)
+    const parent_nav = this.props.navigation.dangerouslyGetParent()
+    this.state = {
+      loading: true,
+      refreshing: false,
+      courseId: parent_nav ? parent_nav.getParam('courseId') : 0,
+      AnnList: [],
+    }
+  }
+
+  async updateAnnList() {
+    let client = new NewE3ApiClient
+    await client.updateCourseAnn(this.state.courseId)
+
+    let courseAnn = await AsyncStorage.getItem('courseAnn' + this.state.courseId)
+    if (!courseAnn) { return }
+    let annList = JSON.parse(courseAnn)
+
+    var renderList: Array<{ key: string, data: ann_type }> = []
+    annList['general'].ann.forEach((ann: ann_type, i: number) => {
+      renderList.push({key: String(i), data: ann})
+    })
+
+    this.setState({ AnnList: renderList })
+  }
+
+  async componentDidMount() {
+    await this.updateAnnList()
+    this.setState({ loading: false })
+  }
+
+  _onRefresh = async () => {
+    this.setState({ refreshing: true })
+    await this.updateAnnList()
+    this.setState({ refreshing: false })
+  }
+
+  _renderItem = ({item}: ListRenderItemInfo<{ key: string, data: ann_type }>) => (
+    <ListItem
+      title={
+        <View>
+          <Text numberOfLines={1} style={styles.title}>
+            {item.data.title}
+          </Text>
+          <Text numberOfLines={1} style={styles.content}>
+            {item.data.content}
+          </Text>
+        </View>
+      }
+      onPress={() => {this.props.navigation.push('Developing')}}
+      topDivider={true}
+      containerStyle={styles.annListItem}
+    />
+  )
+
+  _renderEmptyComponent = () => {
+    if (this.state.loading) {
+      return (
+        <View style={styles.centerContainer}>
+          <Progress.Circle
+            borderWidth={3}
+            size={55}
+            indeterminate={true}
+            thickness={1}
+          />
+        </View>
+      )
+    }
+    else {
+      return (
+        <View style={styles.centerContainer}>
+          <Text style={styles.emptyText}>Nothing here</Text>
+        </View>
+      )
+    }
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>CourseAnnGeneral</Text>
-        <Button title="go" onPress={()=>{this.props.navigation.navigate('Developing')}}></Button>
+        <FlatList
+          contentContainerStyle={{ flexGrow: 1 }}
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.refreshing}
+              onRefresh={this._onRefresh}
+            />
+          }
+          ListEmptyComponent={this._renderEmptyComponent}
+          renderItem={this._renderItem}
+          data={this.state.AnnList}
+        />
       </View>
     );
   }
@@ -37,18 +126,28 @@ export default class CourseAnnGeneralScreen extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  inner: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 16,
+    marginBottom: 2,
+  },
+  content: {
+    fontSize: 14,
+  },
+  annListItem: {
+    paddingHorizontal: 16,
+    paddingTop: 14,
+    paddingBottom: 14,
+  },
+  centerContainer: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  emptyText: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
