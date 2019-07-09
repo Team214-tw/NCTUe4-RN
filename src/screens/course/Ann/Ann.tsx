0c15e2c3dda1cecf50294340c40dcac4bafbfc79
kaiiiz <at881005@gmail.com>
2019-07-09 13:40:18 +0800

    Add abstract CourseAnnScreen

---
+++ src/screens/course/Ann/Ann.tsx
@@ -0,0 +1,137 @@
+import React, { Component } from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  RefreshControl,
+  ListRenderItemInfo,
+  FlatList,
+} from 'react-native'
+import { NavigationScreenProp } from 'react-navigation';
+import * as Progress from 'react-native-progress';
+import { ListItem } from 'react-native-elements';
+
+interface Props {
+  navigation: NavigationScreenProp<States, Props>,
+}
+interface States {
+  loading: boolean,
+  refreshing: boolean,
+  courseId: number,
+  AnnList: Array<{ key: string, data: ann_type, }>,
+}
+
+export default class CourseAnnScreen extends Component<Props, States> {
+
+  constructor(props: Props) {
+    super(props)
+    const parent_nav = this.props.navigation.dangerouslyGetParent()
+    this.state = {
+      loading: true,
+      refreshing: false,
+      courseId: parent_nav ? parent_nav.getParam('courseId') : 0,
+      AnnList: [],
+    }
+  }
+
+  async updateAnnList() { }
+
+  async componentDidMount() {
+    await this.updateAnnList()
+    this.setState({ loading: false })
+  }
+
+  _onRefresh = async () => {
+    this.setState({ refreshing: true })
+    await this.updateAnnList()
+    this.setState({ refreshing: false })
+  }
+
+  _renderItem = ({item}: ListRenderItemInfo<{ key: string, data: ann_type }>) => (
+    <ListItem
+      title={
+        <Text numberOfLines={1} style={styles.title}>
+          {item.data.title}
+        </Text>
+      }
+      subtitle={
+        <Text numberOfLines={1} style={styles.content}>
+          {item.data.content}
+        </Text>
+      }
+      onPress={() => {this.props.navigation.push('Developing')}}
+      topDivider={true}
+      containerStyle={styles.annListItem}
+    />
+  )
+
+  _renderEmptyComponent = () => {
+    if (this.state.loading) {
+      return (
+        <View style={styles.centerContainer}>
+          <Progress.Circle
+            borderWidth={3}
+            size={55}
+            indeterminate={true}
+            thickness={1}
+          />
+        </View>
+      )
+    }
+    else {
+      return (
+        <View style={styles.centerContainer}>
+          <Text style={styles.emptyText}>Nothing here</Text>
+        </View>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          contentContainerStyle={{ flexGrow: 1 }}
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.refreshing}
+              onRefresh={this._onRefresh}
+            />
+          }
+          ListEmptyComponent={this._renderEmptyComponent}
+          renderItem={this._renderItem}
+          data={this.state.AnnList}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inner: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 16,
+    marginBottom: 2,
+  },
+  content: {
+    fontSize: 14,
+  },
+  annListItem: {
+    paddingHorizontal: 16,
+    paddingTop: 14,
+    paddingBottom: 14,
+  },
+  centerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  emptyText: {
+    fontSize: 20,
+  },
+});
--- src/screens/course/Ann/AnnNews.tsx
+++ src/screens/course/Ann/AnnNews.tsx
@@ -1,17 +1,7 @@
-import React, { Component } from 'react'
-import {
-  View,
-  Text,
-  StyleSheet,
-  RefreshControl,
-  ListRenderItemInfo,
-  FlatList,
-} from 'react-native'
 import { NavigationScreenProp } from 'react-navigation';
 import NewE3ApiClient from '../../../client/NewE3ApiClient';
 import AsyncStorage from '@react-native-community/async-storage';
-import * as Progress from 'react-native-progress';
-import { ListItem } from 'react-native-elements';
+import CourseAnnScreen from './Ann';
 
 interface Props {
   navigation: NavigationScreenProp<States, Props>,
@@ -23,18 +13,13 @@ interface States {
   AnnList: Array<{ key: string, data: ann_type, }>,
 }
 
-export default class CourseAnnNewsScreen extends Component<Props, States> {
+export default class CourseAnnNewsScreen extends CourseAnnScreen {
+
   constructor(props: Props) {
     super(props)
-    const parent_nav = this.props.navigation.dangerouslyGetParent()
-    this.state = {
-      loading: true,
-      refreshing: false,
-      courseId: parent_nav ? parent_nav.getParam('courseId') : 0,
-      AnnList: [],
-    }
   }
 
+  // override updateAnnList function
   async updateAnnList() {
     let client = new NewE3ApiClient
     await client.updateCourseAnn(this.state.courseId)
@@ -51,103 +36,4 @@ export default class CourseAnnNewsScreen extends Component<Props, States> {
 
     this.setState({ AnnList: renderList })
   }
-
-  async componentDidMount() {
-    await this.updateAnnList()
-    this.setState({ loading: false })
-  }
-
-  _onRefresh = async () => {
-    this.setState({ refreshing: true })
-    await this.updateAnnList()
-    this.setState({ refreshing: false })
-  }
-
-  _renderItem = ({item}: ListRenderItemInfo<{ key: string, data: ann_type }>) => (
-    <ListItem
-      title={
-        <Text numberOfLines={1} style={styles.title}>
-          {item.data.title}
-        </Text>
-      }
-      subtitle={
-        <Text numberOfLines={1} style={styles.content}>
-          {item.data.content}
-        </Text>
-      }
-      onPress={() => {this.props.navigation.push('Developing')}}
-      topDivider={true}
-      containerStyle={styles.annListItem}
-    />
-  )
-
-  _renderEmptyComponent = () => {
-    if (this.state.loading) {
-      return (
-        <View style={styles.centerContainer}>
-          <Progress.Circle
-            borderWidth={3}
-            size={55}
-            indeterminate={true}
-            thickness={1}
-          />
-        </View>
-      )
-    }
-    else {
-      return (
-        <View style={styles.centerContainer}>
-          <Text style={styles.emptyText}>Nothing here</Text>
-        </View>
-      )
-    }
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <FlatList
-          contentContainerStyle={{ flexGrow: 1 }}
-          refreshControl={
-            <RefreshControl
-              refreshing={this.state.refreshing}
-              onRefresh={this._onRefresh}
-            />
-          }
-          ListEmptyComponent={this._renderEmptyComponent}
-          renderItem={this._renderItem}
-          data={this.state.AnnList}
-        />
-      </View>
-    );
-  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  inner: {
-    flex: 1,
-  },
-  title: {
-    fontSize: 16,
-    marginBottom: 2,
-  },
-  content: {
-    fontSize: 14,
-  },
-  annListItem: {
-    paddingHorizontal: 16,
-    paddingTop: 14,
-    paddingBottom: 14,
-  },
-  centerContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  emptyText: {
-    fontSize: 20,
-  },
-});
